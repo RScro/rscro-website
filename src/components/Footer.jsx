1722e60bda813e6f9ddc4e6e153fe1064671f500
RScro <rscro@protonmail.com>
2023-10-12 21:07:46 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-b-transparent border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between mx-auto sm:flex-none sm:text-center">
+      <div className="container p-12 flex justify-between mx-auto sm:flex-none sm:text-center flex-wrap">
         <span>
           <img src={logo} width={100} height={100} />
         </span>
