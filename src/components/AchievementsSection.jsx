ee1aa9303a1f4cf013bbafa502a61d5bc6fa55a2
RScro <rscro@protonmail.com>
2023-10-12 19:02:16 +0200

    bug fixes

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -25,12 +25,15 @@ const achievementsList = [
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 md:px-16 flex sm:flex-row items-center justify-between">
+      <div className="border-[#33353F] border rounded-md py-8 md:px-16 flex flex-wrap items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+              className={`flex flex-col items-center justify-center mx-4 my-4 sm:my-0 ${
+                index === 3 ? "xl:order-2" : "" // Adjust the order for "5 Years" element
+              } flex-auto`}
+              style={{ flex: "1 1%" }} // Keep four columns on larger screens
             >
               <h2 className="text-white text-2xl sm:text-4xl font-bold flex flex-row">
                 {achievement.prefix}
